dc97b4d5abd4dbc6f1ee4fe026ab939d990d5820
umadevic <51950421+umadevic@users.noreply.github.com>
2019-07-03 15:04:39 +0530

    Update 52.py

--- 52.py
+++ 52.py
@@ -1,3 +1,4 @@
-xy1=int(input())
-nm2=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
-print(nm2[xy1])
+nm1=int(input())
+kl2=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+print(kl2[nm1])
+
